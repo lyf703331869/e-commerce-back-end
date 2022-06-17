bf8fc6d84ab084a5ad68fe68d89458000875c0f9
lyf703331869 <yliu52@nyit.edu>
2022-06-16 21:24:07 -0400

    Defined relationships between different models

--- models/index.js
+++ models/index.js
@@ -5,12 +5,31 @@ const Tag = require("./Tag");
 const ProductTag = require("./ProductTag");
 
 // Categories have many Products
+Category.hasMany(Product, {
+  foreignKey: "category_id",
+  onDelete: "CASCADE",
+});
 
 // Products belongsTo Category
+Product.belongsTo(Category, {
+  foreignKey: "category_id",
+});
 
 // Products belongToMany Tags (through ProductTag)
+Product.belongsTo(Tag, {
+  through: {
+    model: ProductTag,
+    unique: false,
+  },
+});
 
 // Tags belongToMany Products (through ProductTag)
+Tag.belongsTo(Product, {
+  through: {
+    model: ProductTag,
+    unique: false,
+  },
+});
 
 module.exports = {
   Product,
